2620326e309bb42056ad5fded5ce8970e0afb606
abrahampo1 <abraham@cpsoftware.es>
2020-12-01 20:05:17 +0100

    boton git pull

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php?>
+<?php
+if(isset($_POST["update"])){
+    exec("git pull");
+}
+?>
 <html>
 HOLA
+<form method="post">
+<input type="hidden" name="update" value="">
+<button type="submit">GIT PULL</button>
+</form>
 </html>
